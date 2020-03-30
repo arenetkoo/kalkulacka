3cff8ac8ad2251440a2a6730ee12dea01ebb9d8e
arenetkoo <61801474+arenetkoo@users.noreply.github.com>
2020-03-30 11:15:24 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double cislo1;
+            double cislo2;
+            double result1;
+            string znamenko;
+            Console.WriteLine("Zadej znaménko");
+            znamenko = Console.ReadLine();
+            if(znamenko == "+") 
+            {
+                Console.WriteLine("Zadej prvni cislo");
+                cislo1 = int.Parse(Console.ReadLine());
+                Console.WriteLine("Zadej druhe cislo");
+                cislo2 = int.Parse(Console.ReadLine());
+                result1 = cislo1 + cislo2;
+                Console.WriteLine("Vysledek je " + result1);
+            }
+            if(znamenko == "-")
+            {
+                Console.WriteLine("Zadej prvni cislo");
+                cislo1 = int.Parse(Console.ReadLine());
+                Console.WriteLine("Zadej druhe cislo");
+                cislo2 = int.Parse(Console.ReadLine());
+                result1 = cislo1 - cislo2;
+                Console.WriteLine("Vysledek je " + result1);
+            }
+            if (znamenko == "*")
+            {
+                Console.WriteLine("Zadej prvni cislo");
+                cislo1 = int.Parse(Console.ReadLine());
+                Console.WriteLine("Zadej druhe cislo");
+                cislo2 = int.Parse(Console.ReadLine());
+                result1 = cislo1 * cislo2;
+                Console.WriteLine("Vysledek je " + result1);
+            }
+            if (znamenko == "/")
+            {
+                Console.WriteLine("Zadej prvni cislo");
+                cislo1 = int.Parse(Console.ReadLine());
+                Console.WriteLine("Zadej druhe cislo");
+                cislo2 = int.Parse(Console.ReadLine());
+                result1 = cislo1 / cislo2;
+                Console.WriteLine("Vysledek je " + result1);
+            }
+            
+            Console.ReadKey();
+        }
+    }
+}
